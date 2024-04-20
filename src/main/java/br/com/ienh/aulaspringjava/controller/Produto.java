8bf86764dc842396e23ac27cdf5d861888fea1b9
RedKatzen <gabrieldeavilaf@gmail.com>
2024-04-19 21:49:29 -0300

    Fazendo (parte) do exercício IMC (1); Adicionando o Model (service\IMCService) - Regras de negócio

--- src/main/java/br/com/ienh/aulaspringjava/controller/Produto.java
+++ src/main/java/br/com/ienh/aulaspringjava/controller/Produto.java
@@ -3,21 +3,27 @@ package br.com.ienh.aulaspringjava.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/produto")
 public class Produto {
 
-    @GetMapping("/visualizar")
-    public String produto(int id, String tipo, Model model) {
-        System.out.println("ID: " + id);
-        System.out.println("Tipo: " + tipo);
+//    @GetMapping("/visualizar")
+//    public String produto(int id, String tipo, Model model) {
+//        System.out.println("ID: " + id);
+//        System.out.println("Tipo: " + tipo);
+//        model.addAttribute("id", id);
+//        model.addAttribute("tipo", tipo);
+//        return "produto";
+//    }
+
+    @GetMapping("/teste/{id}/{tipo}")
+    public String teste(@PathVariable int id, @PathVariable String tipo, Model model) {
         model.addAttribute("id", id);
         model.addAttribute("tipo", tipo);
         return "produto";
     }
-
-
 
 }
