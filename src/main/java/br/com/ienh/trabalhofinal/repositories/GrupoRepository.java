28bb26f2cd99eb522ae180f100db1a2b21c40c64
RedKatzen <gabrieldeavilaf@gmail.com>
2024-06-28 21:31:49 -0300

    Arrumando Contato e Cliente; nenhuma tela funcionando

--- src/main/java/br/com/ienh/trabalhofinal/repositories/GrupoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.ienh.trabalhofinal.repositories;
-
-import br.com.ienh.trabalhofinal.entities.Grupo;
-import org.springframework.data.repository.CrudRepository;
-
-public interface GrupoRepository extends CrudRepository<Grupo, Integer>{
-    public Grupo findByNome(String nome);
-}
--- src/main/java/br/com/ienh/trabalhofinal/repositories/MarcaRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.ienh.trabalhofinal.repositories;
-
-import br.com.ienh.trabalhofinal.entities.Marca;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MarcaRepository extends CrudRepository<Marca, Integer>{
-    public Marca findByNome(String nome);
-}
